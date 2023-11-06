2b4f8c8df35e1d9145fbb19b24c31df0faff1368
m2300813 <m2300813@D1006Z-011C>
2023-11-06 14:35:06 +0000

    Add project files.

---
+++ Testing/Testing.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	cout << "Choose a number between 1 and 100. \n";
+	int num{ 50 };
+	int input;
+		cin >> input;
+		if (num > input)
+		{
+			cout << "Your number is lower than 50. \n";
+		}
+		else if (num > input)
+		{
+			cout << "Your number is larger than 50. \n";
+		}
+
+
+
+}
+
+
